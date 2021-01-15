c0973ce22b1dbc8ac8c583f8198982cf81588d68
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-15 12:08:02 +0200

    show method #1 of how to test an asynchronous function

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -4,13 +4,29 @@ test("should verify that 2 + 2 equals 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
-test(`should return John Doe's user`, () => {
-  const user = {
-    firstName: "John",
-    lastName: "Doe",
-  };
+test(`should _asynchronously_ fetch Leanne Graham's user`, () => {
+  /*
+  https://jestjs.io/docs/en/asynchronous >>
+  When you have [JavaScript] code that runs asynchronously,
+  Jest needs to know when the code it is testing has completed,
+  before it can move on to another test.
+  Jest has several ways to handle this.
 
-  expect(functions.createUser()).not.toBe(user);
+  [Read the link for examples of those several ways.]
+  */
 
-  expect(functions.createUser()).toEqual(user);
+  /* Verify that a certain # of assertions are called */
+  expect.assertions(1);
+
+  const promise = functions.fetchUser();
+
+  /*
+  If you omit [the following] `return` statement,
+  [the] test will complete before
+    the promise returned from [`functions.fetchUser`] resolves.
+    and `then()` has a chance to execute [its] callback.
+  */
+  return promise.then((data) => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
 });
