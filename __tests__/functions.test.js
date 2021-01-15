4592542a02f8bd7873567a4e08c1f317b2c4ab39
Kaloyan Marinov <kallo.marinov@gmail.com>
2021-01-15 11:54:31 +0200

    demonstrate the difference between Jest's toBe and toEqual matchers

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -3,3 +3,14 @@ const functions = require("../src/functions");
 test("should verify that 2 + 2 equals 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test(`should return John Doe's user`, () => {
+  const user = {
+    firstName: "John",
+    lastName: "Doe",
+  };
+
+  expect(functions.createUser()).not.toBe(user);
+
+  expect(functions.createUser()).toEqual(user);
+});
--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,12 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
+  createUser: () => {
+    const user = {
+      firstName: "John",
+      lastName: "Doe",
+    };
+    return user;
+  },
 };
 
 module.exports = functions;
